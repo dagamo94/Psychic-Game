59933b13c363b5cb533f0cd30ae00db870bda3a7
Surface <da.ga.mo.94@gmail.com>
2019-05-31 17:47:11 -0600

    game is fully functional

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -10,12 +10,20 @@ var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
 var userGuesses = [];
-var cpuGuess = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var cpuGuess = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var computerChoice = cpuChoice();
 
-function cpuChoice(){
+function cpuChoice() {     //generates a random number every time a key is pressed but is only called whenever the game needs to be reset
     return cpuGuess[Math.floor(Math.random() * cpuGuess.length)];
 }
+
+function resetVariables(){
+    computerChoice = cpuChoice();
+    guessesLeft = 9;
+    userGuesses = [];
+    userGuessesText.textContent = "";
+}
+
 
 console.log("Initial CPU choice: " + computerChoice);
 
@@ -24,64 +32,49 @@ document.onkeyup = function (event) {
 
     var userInput = event.key;
 
-    //generates a random number every time a key is pressed but is only called whenever the game needs to be reset
-    // function cpuChoice(){
-    //     return cpuGuess[Math.floor(Math.random() * cpuGuess.length)];
-    // }
-    function printUserGuesses(arr){
-        for(var i = 0; i < userGuesses.length; i++){ //prints current array values to the console
+   
+    function printUserGuesses(arr) {
+        for (var i = 0; i < userGuesses.length; i++) { //prints current user guesses array values to the console
             return userGuesses[i] + ",";
         }
     }
 
-    if(cpuGuess.includes(userInput) && userGuesses.indexOf(userInput) < 0){
 
-        if(userInput === computerChoice && guessesLeft > 1){
+
+    if (cpuGuess.includes(userInput) && userGuesses.indexOf(userInput) < 0) {
+
+        if (userInput === computerChoice && guessesLeft > 1) {
             wins++;
-            guessesLeft = 9;
-            userGuesses = [];
-            computerChoice = cpuChoice();
-            console.log("you won!");
             
-        }else if(userInput != computerChoice && guessesLeft > 1){
-            guessesLeft--;
-            userGuesses.push(userInput);            
-            console.log("choose again");
+            resetVariables();
+            console.log("you won!");
 
-        }else{
-            computerChoice = cpuChoice();
+        } else if (userInput != computerChoice && guessesLeft > 1) {
+            guessesLeft--;
+            userGuesses.push(userInput);
+            console.log("choose again");
+            userGuessesText.append(event.key);
+
+
+        } else {
+            resetVariables();
             losses++;
-            guessesLeft = 9;
-            userGuesses = [];
         }
 
-    }else{
+    } else {
         alert("Letter has already been used or press a letter from the alphabet (a-z)");
-    }
-
-    function printTo(){
-        
     }
 
     winsText.textContent = wins;
     lossesText.textContent = losses;
     guessesLeftText.textContent = guessesLeft;
-    userGuessesText.textContent = userGuesses.forEach(function(item){
-        
-        var printText = item;
-        console.log("user guesses: " + printText);
-        return printText + ", ";
-    });
+
 
     console.log("Wins: " + wins);
     console.log("Losses: " + losses);
     console.log("Guesses left: " + guessesLeft);
-    console.log("CPU guess: " + computerChoice); //display the computer choice
-    // for(var i = 0; i < userGuesses.length; i++){ //prints current array values to the console
-    //     console.log("Current user guesses: " + userGuesses[i]);
-    // }
-    //console.log(printUserGuesses());
-    
+    console.log("CPU guess: " + computerChoice); 
+
 
     console.log("\n");
 
